6951a91ba9d24d3df8d01fdf9670eef16967ed9e
Anurag Velekat <92157150+AnuragVelekat@users.noreply.github.com>
2023-11-02 21:11:46 +0530

    removed random variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,4 @@
 import reactImg from "../assets/react-core-concepts.png";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
 
 function Header() {
   return (
@@ -11,8 +6,8 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[getRandomInt(reactDescriptions.length)]} React
-        concepts you will need for almost any app you are going to build!
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
